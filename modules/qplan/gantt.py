2824d1383e8a8e728da5fdf9a39d3ac4e59c9a47
Avinash Baliga <fifoforlifo@gmail.com>
2015-12-07 09:34:05 -0600

    gantt: connector lines for all deps

--- modules/qplan/gantt.py
+++ modules/qplan/gantt.py
@@ -25,13 +25,22 @@ def plot_gantt(schedule_items, critical_path):
         item = items[ii]
         bar_color = 'red' if item.task.name in crit_path_names else 'blue'
         ax.barh(y_pos[ii], item.end_time - item.start_time, left=item.start_time, height=0.3, align='center', color=bar_color, alpha=0.75)
-        if item.pred_task:
+        for dep_name in item.task.deps:
+            dep_item = schedule_items[dep_name]
+            dep_color = 'red' if dep_name == item.pred_task.name else 'blue'
             plt.vlines(
                 item.start_time,
                 y_pos[name_to_idx[item.task.name]],
-                y_pos[name_to_idx[item.pred_task.name]],
-                colors = bar_color,
+                y_pos[name_to_idx[dep_name]],
+                colors = dep_color,
                 linestyles='dotted')
+            plt.hlines(
+                y_pos[name_to_idx[dep_name]],
+                dep_item.end_time,
+                item.start_time,
+                colors = dep_color,
+                linestyles='dotted')
+
 
     plt.gcf().tight_layout()
     plt.show()
